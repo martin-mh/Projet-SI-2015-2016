ea9e48c7822bf6e0347218bcaa6ed3236d4f87d7
Martin Hammerchmidt <martin.hammerchmidt@gmail.com>
2015-12-11 11:34:33 +0100

    Updating main to reflect others changes

--- main.ino
+++ main.ino
@@ -1,25 +1,62 @@
+#include <LiquidCrystal.h>
+
 #include "parameters.h"
 
-#include "dht22.h"
+#include "debug.h"
 
-DhtManager* dht;
+#include "dht22.h"
+#include "webserver.h"
+
+LiquidCrystal lcd(12,11,5,4,3,2);
+const int lcdCol = 16;
+const int lcdRow = 2;
+
+byte mac[] = { MAC };  
+
+DhtManager * dht;
+WebServer * ws;
 
 void setup() 
 {
-    Serial.begin(9600); 
+    lcd.begin(lcdCol, lcdRow);
+    lcd.blink();
+    lcd.print("Chargement...");
+    
+    Serial.begin(9600);
     Serial.println();
+    Debug::setup(&Serial);
 
     dht = new DhtManager();
+    
+    ws = new WebServer();
+    Debug::println("WebServer created");
+    ws->init(mac);
+    Debug::println("WebServer inited");
+
+    lcd.clear();
+    lcd.noBlink();
+    lcd.setCursor(0, 0);
+    lcd.print(ws->getLocalIp());
+    ws->setDatas(dht->getHumidity(), dht->getTemperature());
 }
 
 void loop() 
 {
     dht->update();
+    ws->loop();
     
     Serial.print("Humidity: "); 
-    Serial.print(dht->getHumidity());
+    Serial.print(*dht->getHumidity());
     Serial.print(" %\t");
     Serial.print("Temperature: "); 
-    Serial.print(dht->getTemperature());
+    Serial.print(*dht->getTemperature());
     Serial.println(" *C");
+
+    lcd.setCursor(0, 1);
+    lcd.print(*dht->getTemperature());
+    lcd.print(" *C ");
+    lcd.print(*dht->getHumidity());
+    lcd.print(" %     ");
+
+    //delay(1000);
 }
